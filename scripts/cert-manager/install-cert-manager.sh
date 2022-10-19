9afcffdc698255235ace2a8cb5abdd1bf9dc835b
Michael Marshall <mmarshall@apache.org>
2022-10-19 15:11:45 -0500

    Upgrade to 1.7.3
    Here is the relevant documentation for k8s compatibility:
    https://cert-manager.io/docs/installation/supported-releases/

--- scripts/cert-manager/install-cert-manager.sh
+++ scripts/cert-manager/install-cert-manager.sh
@@ -24,7 +24,7 @@ set -e
 
 NAMESPACE=cert-manager
 NAME=cert-manager
-VERSION=v1.10.0
+VERSION=v1.7.3
 
 # Install cert-manager CustomResourceDefinition resources
 echo "Installing cert-manager CRD resources ..."
