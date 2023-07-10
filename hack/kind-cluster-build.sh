c793b259f2f0395c96c7a6aece8a18adf85a5c9d
Lari Hotari <lhotari@apache.org>
2023-07-10 18:53:38 +0300

    Upgrade to k8s 1.24

--- hack/kind-cluster-build.sh
+++ hack/kind-cluster-build.sh
@@ -83,7 +83,7 @@ done
 
 clusterName=${clusterName:-pulsar-dev}
 nodeNum=${nodeNum:-6}
-k8sVersion=${k8sVersion:-v1.20.15}
+k8sVersion=${k8sVersion:-v1.24.15}
 volumeNum=${volumeNum:-9}
 
 echo "clusterName: ${clusterName}"
