724f2d9ceaf5e9dcda6be41d3df7f437c2182bbc
glecroc <guillaume.lecroc@cnp.fr>
2025-05-12 15:44:05 +0200

    test truncate command

--- .ci/helm.sh
+++ .ci/helm.sh
@@ -494,8 +494,10 @@ function ci::create_openid_resources() {
     echo "Creating openid resources for ${component}"
 
     local client_id=pulsar-${component}
-    which tr
-    which truncate
+    set +e
+    tr -dc '[:alnum:]' < /dev/urandom | head -c 32
+    cat /dev/urandom | tr -dc '[:alnum:]' | head -c 32
+    set -e
     local client_secret=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 32)
 
     # Create the client credentials secret
