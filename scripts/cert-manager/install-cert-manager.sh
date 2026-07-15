594011278b37ae3aedcd0a5c5b2c62eb4b262949
Lari Hotari <lhotari@apache.org>
2026-07-15 12:59:16 +0300

    Use certmanager v1.21.0 for testing with k8s 1.36.1

--- scripts/cert-manager/install-cert-manager.sh
+++ scripts/cert-manager/install-cert-manager.sh
@@ -24,7 +24,7 @@ set -e
 NAMESPACE=cert-manager
 NAME=cert-manager
 # check compatibility with k8s versions from https://cert-manager.io/docs/installation/supported-releases/
-VERSION=v1.12.17
+VERSION=${1:-v1.12.17}
 
 # Install cert-manager CustomResourceDefinition resources
 echo "Installing cert-manager CRD resources ..."
